4a2ecd620027cb8e517a9833503490729386e59f
Roman Smirnov <roman@railsdog.com>
2010-10-29 18:18:17 +0400

    Fixed issue with affiliate link in email body; Fixed issue with tracking multiple email addresses.

--- app/controllers/email_sender_controller_decorator.rb
+++ app/controllers/email_sender_controller_decorator.rb
@@ -2,9 +2,12 @@ EmailSenderController.class_eval do
   private
   
   def after_delivering_affiliate_mail
-    if Affiliate.where(:affiliate_email => @mail_to_friend.recipient_email).count == 0
-      affiliate = current_user.find_or_create_affiliate
-      affiliate.update_attribute(:affiliate_email, @mail_to_friend.recipient_email)
+    emails = @mail_to_friend.recipient_email.split(/,\s?/)
+    emails.each do |email|
+      if Affiliate.where(:affiliate_email => email).count == 0
+        affiliate = current_user.find_or_create_affiliate
+        affiliate.update_attribute(:affiliate_email, email)
+      end
     end
   end
 end
--- app/models/affiliate.rb
+++ app/models/affiliate.rb
@@ -5,33 +5,8 @@ class Affiliate < ActiveRecord::Base
   def name
     I18n.t(:affiliate_program)
   end
-end
-
-=begin
-class Affiliate
-  extend ActiveModel::Naming
-
-  def initialize(user)
-    @user = user
-  end
   
-  def to_param
-    @user.ref_id
-  end
-  
-  def name
-    I18n.t(:affiliate_program)
-  end
-  
-  class << self
-
-    def find_by_id(id)
-      user = User.find_by_ref_id(id)
-      Affiliate.new(user)
-    end
-  
-    alias :find :find_by_id
-
+  def ref_id
+    partner ? partner.ref_id : ''
   end
 end
-=end
