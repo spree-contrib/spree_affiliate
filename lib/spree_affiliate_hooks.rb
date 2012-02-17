b03622a8a958fd853e2c6128c12a5044e148ed6f
Gary <gary@newtechfusion.com>
2012-02-17 18:51:11 +0530

    when i run rake spree_store_credits:install that time error is occurred [DEPRECATION] `insert_after` hook method is deprecated, replace hook call with: Deface::Override.new(:virtual_path => "admin/configurations/index",                      :name => "converted_admin_configurations_menu_404040658",                      :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",                      :text => "<%= configurations_menu_item(I18n.t('captcha.captcha_settings'), admin_captcha_settings_path, I18n.t('captcha.manage_keys')) %>",                      :disabled => false)

--- lib/spree_affiliate_hooks.rb
+++ lib/spree_affiliate_hooks.rb
@@ -1,7 +1,10 @@
 class SpreeAffiliateHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_configurations_menu do
-    "<%= configurations_menu_item(I18n.t('affiliate_settings'), admin_affiliate_settings_path, I18n.t('manage_affiliate_settings')) %>"
-  end
+  Deface::Override.new(:virtual_path => "admin/configurations/index",
+                     :name => "converted_admin_configurations_menu_404040658",
+                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                     :text => "<%= configurations_menu_item(I18n.t('captcha.captcha_settings'), admin_captcha_settings_path, I18n.t('captcha.manage_keys')) %>",
+                     :disabled => false)
+
   
   insert_before :account_my_orders, :partial => 'users/affiliate'
   
