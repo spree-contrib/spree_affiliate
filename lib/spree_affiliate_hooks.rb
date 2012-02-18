68fecd597f42241924e4c9a34b26dfafa8d2c66a
Gary <gary@newtechfusion.com>
2012-02-18 16:59:11 +0530

    [DEPRECATION] 'insert_after', 'insert_before' replace with Deface gem

--- lib/spree_affiliate_hooks.rb
+++ lib/spree_affiliate_hooks.rb
@@ -1,9 +1,14 @@
 class SpreeAffiliateHooks < Spree::ThemeSupport::HookListener
-  insert_after :admin_configurations_menu do
-    "<%= configurations_menu_item(I18n.t('affiliate_settings'), admin_affiliate_settings_path, I18n.t('manage_affiliate_settings')) %>"
-  end
-  
-  insert_before :account_my_orders, :partial => 'users/affiliate'
+  Deface::Override.new(:virtual_path => "admin/configurations/index",
+                     :name => "converted_admin_configurations_menu_242938330",
+                     :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
+                     :text => "<%= configurations_menu_item(I18n.t('affiliate_settings'), admin_affiliate_settings_path, I18n.t('manage_affiliate_settings')) %>",
+                     :disabled => false)
+  Deface::Override.new(:virtual_path => "users/show",
+                     :name => "converted_account_my_orders_347563100",
+                     :insert_before => "[data-hook='account_my_orders'], #account_my_orders[data-hook]",
+                     :partial => "users/affiliate",
+                     :disabled => false)
   
   replace :send_mail_fields, :partial => 'email_sender/fields'
 end
