233550d9dcd2b45a3bb2779f7118b5420611b9b2
David North <davidnorth@railsdog.com>
2012-03-08 17:44:27 +0000

    Use Spree 1.0 style preferences

---
+++ lib/spree_affiliate/configuration.rb
@@ -0,0 +1,8 @@
+class SpreeAffiliate::Configuration < Spree::Preferences::Configuration
+
+  preference :sender_credit_on_purchase_amount, :decimal, :default => 0.0
+  preference :sender_credit_on_register_amount, :decimal, :default => 0.0
+  preference :recipient_credit_on_register_amount, :decimal, :default => 0.0
+  preference :recipient_credit_on_purchase_amount, :decimal, :default => 0.0
+
+end
