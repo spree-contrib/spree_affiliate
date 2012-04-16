97b34f5148573378bdf15563c8c43f85dbbbe5c4
Graham Melcher <melcher@gmail.com>
2012-04-15 22:20:40 -0700

    Namespace configuration

--- lib/spree_affiliate/configuration.rb
+++ lib/spree_affiliate/configuration.rb
@@ -1,8 +1,16 @@
-class SpreeAffiliate::Configuration < Spree::Preferences::Configuration
+module Spree
+  class AuthConfiguration < Preferences::Configuration
+    preference :registration_step, :boolean, :default => true
+    preference :signout_after_password_change, :boolean, :default => true
+  end
+end
 
-  preference :sender_credit_on_purchase_amount, :decimal, :default => 0.0
-  preference :sender_credit_on_register_amount, :decimal, :default => 0.0
-  preference :recipient_credit_on_register_amount, :decimal, :default => 0.0
-  preference :recipient_credit_on_purchase_amount, :decimal, :default => 0.0
 
+module Spree
+  class AffiliateConfiguration < Spree::Preferences::Configuration
+    preference :sender_credit_on_purchase_amount, :decimal, :default => 0.0
+    preference :sender_credit_on_register_amount, :decimal, :default => 0.0
+    preference :recipient_credit_on_register_amount, :decimal, :default => 0.0
+    preference :recipient_credit_on_purchase_amount, :decimal, :default => 0.0
+  end
 end
