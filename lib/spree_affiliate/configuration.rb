ff841586173e095795c3d367aacf90c6e2273798
Graham Melcher <melcher@gmail.com>
2012-04-29 16:26:40 -0700

    Don't change our registration preferences.

--- lib/spree_affiliate/configuration.rb
+++ lib/spree_affiliate/configuration.rb
@@ -1,12 +1,4 @@
 module Spree
-  class AuthConfiguration < Preferences::Configuration
-    preference :registration_step, :boolean, :default => true
-    preference :signout_after_password_change, :boolean, :default => true
-  end
-end
-
-
-module Spree
   class AffiliateConfiguration < Spree::Preferences::Configuration
     preference :sender_credit_on_purchase_amount, :decimal, :default => 0.0
     preference :sender_credit_on_register_amount, :decimal, :default => 0.0
