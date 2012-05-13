d4eeaa2b974a01e8c20807d837ef847d81a6fc98
Graham Melcher <melcher@gmail.com>
2012-05-13 12:24:46 -0700

    Sender must be a user, not an affiliate

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -7,7 +7,7 @@ Spree::CheckoutController.class_eval do
     session[:order_id] = nil
 
     if current_user && current_user.affiliate_partner && current_user.orders.where(:state => 'complete').count == 1
-      sender = current_user.affiliate_partner
+      sender = current_user.affiliate_partner.partner
 
       #create credit (if required)
       create_affiliate_credits(sender, current_user, "purchase")
