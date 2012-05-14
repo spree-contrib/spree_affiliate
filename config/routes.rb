157d14ae4d252da965a1555b2515fd2aae87ef10
Bounmy Stephane <stephanebounmy@gmail.com>
2012-05-14 22:35:33 +0200

    set spree_affiliate as real engine and added generator

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Spree::Core::Engine.routes.append do
+Spree::Core::Engine.routes.prepend do
   namespace :admin do
     resource :affiliate_settings
   end
---
+++ lib/spree_affiliate/engine.rb
@@ -0,0 +1,16 @@
+module SpreeAffiliate
+  class Engine < Rails::Engine
+    engine_name 'spree_affiliate'
+
+    config.autoload_paths += %W(#{config.root}/lib)
+
+    def self.activate
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.env == "production" ? require(c) : load(c)
+      end
+
+    end
+
+    config.to_prepare &method(:activate).to_proc
+  end
+end
