a7d2dd7f944d33380ddec1e8f9cb1ff7db56e5d4
Stephane Bounmy <stephanebounmy@gmail.com>
2012-09-10 23:34:50 +0200

    quick patch for spree_social

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -3,20 +3,4 @@ Spree::UserRegistrationsController.class_eval do
 
   after_filter :check_affiliate, :only => :create
 
-  private
-
-  def check_affiliate
-    return if cookies[:ref_id].blank? || @user.invalid?
-    sender = Spree::User.find_by_ref_id(cookies[:ref_id])
-
-    if sender
-      sender.affiliates.create(:user_id => @user.id)
-
-      #create credit (if required)
-      @credited = create_affiliate_credits(sender, @user, "register")
-    end
-
-    #destroy the cookie, as the affiliate record has been created.
-    cookies[:ref_id] = nil
-  end
 end
--- lib/affiliate_credits.rb
+++ lib/affiliate_credits.rb
@@ -27,4 +27,18 @@ module AffiliateCredits
     affiliate.events.create({:reward => credit, :name => event, :user => user}, :without_protection => true)
   end
 
+  def check_affiliate
+    return if cookies[:ref_id].blank? || @user.invalid?
+    sender = Spree::User.find_by_ref_id(cookies[:ref_id])
+
+    if sender
+      sender.affiliates.create(:user_id => @user.id)
+
+      #create credit (if required)
+      @credited = create_affiliate_credits(sender, @user, "register")
+    end
+
+    #destroy the cookie, as the affiliate record has been created.
+    cookies[:ref_id] = nil
+  end
 end
