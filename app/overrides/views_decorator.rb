7160223e8881e3326e575145b7a59c814643568d
Bryan Mahoney <bryan@goDynamo.com>
2013-02-21 09:11:52 -0500

    Corrects deface override

--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
-		 :name => "converted_admin_configurations_menu_294924217",
-		 :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
-		 :text => "<%= configurations_menu_item(I18n.t('affiliate_settings'), spree.admin_affiliate_settings_path, I18n.t('manage_affiliate_settings')) %>")
+		 :name => "affiliates_admin_configuration_menu",
+     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
+     :text => "<%= configurations_sidebar_menu_item t(:affiliate_settings), spree.admin_affiliate_settings_path %>")
 
 Deface::Override.new(:virtual_path => "spree/users/show",
 		 :name => "converted_account_my_orders_69371225",
