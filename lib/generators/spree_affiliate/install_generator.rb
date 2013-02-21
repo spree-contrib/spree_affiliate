a965a910bef5a710e6241747a9e6d3816dabb39f
Bryan Mahoney <bryan@goDynamo.com>
2013-02-21 09:00:08 -0500

    Updates install generator

---
+++ lib/generators/spree_affiliate/install_generator.rb
@@ -0,0 +1,18 @@
+module SpreeAffiliate
+  module Generators
+    class InstallGenerator < Rails::Generators::Base
+      def add_migrations
+        run 'bundle exec rake railties:install:migrations FROM=spree_affiliate'
+      end
+
+      def run_migrations
+         res = ask "Would you like to run the migrations now? [Y/n]"
+         if res == "" || res.downcase == "y"
+           run 'bundle exec rake db:migrate'
+         else
+           puts "Skipping rake db:migrate, don't forget to run it!"
+         end
+      end
+    end
+  end
+end
