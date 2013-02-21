877155dc5e9a705e5e8aeefcdf75c4a05c9f850e
Bryan Mahoney <bryan@goDynamo.com>
2013-02-21 09:07:42 -0500

    Updates override for configuration menu

--- app/overrides/views_decorator.rb
+++ app/overrides/views_decorator.rb
@@ -1,4 +1,4 @@
-Deface::Override.new(:virtual_path => "spree/admin/configurations/index",
+Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
 		 :name => "converted_admin_configurations_menu_294924217",
 		 :insert_after => "[data-hook='admin_configurations_menu'], #admin_configurations_menu[data-hook]",
 		 :text => "<%= configurations_menu_item(I18n.t('affiliate_settings'), spree.admin_affiliate_settings_path, I18n.t('manage_affiliate_settings')) %>")
