5bf688c6ab1f1061eed8b4734b1c6d4b5352e53f
Josh Nussbaum <joshnuss@gmail.com>
2013-03-08 22:29:54 -0500

    rename helper and add link_to_referral_url helper

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,5 +1,11 @@
 Spree::BaseHelper.class_eval do
-  def referral_link(referrer)
+  def referral_url(referrer)
     root_url(:ref_id => referrer.ref_id)
+  end
+
+  def link_to_referral_url(referrer, options = {})
+    url = referral_url(referrer)
+
+    link_to(url, url, options)
   end
 end
