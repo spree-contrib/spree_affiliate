17a47e757fd67dfeee3e345852daa60f39f120d2
Bryan Mahoney <bryan@goDynamo.com>
2013-03-11 23:15:38 -0300

    Removes protection when creating associations

--- lib/affiliate_credits.rb
+++ lib/affiliate_credits.rb
@@ -4,18 +4,19 @@ module AffiliateCredits
   def create_affiliate_credits(sender, recipient, event)
     #check if sender should receive credit on affiliate register
     if sender_credit_amount = SpreeAffiliate::Config["sender_credit_on_#{event}_amount".to_sym] and sender_credit_amount.to_f > 0
-      credit = Spree::StoreCredit.create(:amount => sender_credit_amount,
+      credit = Spree::StoreCredit.create({:amount => sender_credit_amount,
                          :remaining_amount => sender_credit_amount,
-                         :reason => "Affiliate: #{event}", :user => sender)
+                         :reason => "Affiliate: #{event}", :user => sender}, :without_protection => true)
 
       log_event recipient.affiliate_partner, sender, credit, event
     end
 
+
     #check if affiliate should recevied credit on sign up
     if recipient_credit_amount = SpreeAffiliate::Config["recipient_credit_on_#{event}_amount".to_sym] and recipient_credit_amount.to_f > 0
-      credit = Spree::StoreCredit.create(:amount => recipient_credit_amount,
+      credit = Spree::StoreCredit.create({:amount => recipient_credit_amount,
                          :remaining_amount => recipient_credit_amount,
-                         :reason => "Affiliate: #{event}", :user => recipient)
+                         :reason => "Affiliate: #{event}", :user => recipient}, :without_protection => true)
 
       log_event recipient.affiliate_partner, recipient, credit, event
     end
@@ -23,7 +24,22 @@ module AffiliateCredits
   end
 
   def log_event(affiliate, user, credit, event)
-    affiliate.events.create(:reward => credit, :name => event, :user => user)
+    affiliate.events.create({:reward => credit, :name => event, :user => user}, :without_protection => true)
   end
 
+  def check_affiliate
+    @user.reload if @user.present? and not @user.new_record?
+    return if cookies[:ref_id].blank? || @user.nil? || @user.invalid?
+    sender = Spree.user_class.find_by_ref_id(cookies[:ref_id])
+
+    if sender
+      sender.affiliates.create(:user_id => @user.id)
+
+      #create credit (if required)
+      @credited = create_affiliate_credits(sender, @user, "register")
+    end
+
+    #destroy the cookie, as the affiliate record has been created.
+    cookies[:ref_id] = nil
+  end
 end
