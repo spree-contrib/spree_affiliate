2bf122b8163b2af963defde67b9ffd2be5964e91
Bryan Mahoney <bryan@goDynamo.com>
2013-03-11 23:21:11 -0300

    Adds missing attr_accessible

--- app/models/spree/affiliate.rb
+++ app/models/spree/affiliate.rb
@@ -1,4 +1,6 @@
 class Spree::Affiliate < ActiveRecord::Base
+
+  attr_accessible :user_id
   belongs_to :partner, :class_name => "Spree::User", :foreign_key => :partner_id
   belongs_to :user, :class_name => 'Spree::User'
   has_many :events, :class_name => "Spree::AffiliateEvent"
