3b3dc6758980b84e7e5c23cda660716d9fcfd367
wojciech.zywno <wojciech.zywno@goodylabs.com>
2013-07-23 15:24:41 +0200

    upgrade to Spree 2.0

--- lib/affiliate_credits.rb
+++ lib/affiliate_credits.rb
@@ -1,22 +1,27 @@
 module AffiliateCredits
+
   private
 
-    def create_affiliate_credits(sender, recipient, event)
+    def create_affiliate_credits(sender, recipient, event, extra = {})
       #check if sender should receive credit on affiliate register
       if sender_credit_amount = SpreeAffiliate::Config["sender_credit_on_#{event}_amount".to_sym] and sender_credit_amount.to_f > 0
         credit = Spree::StoreCredit.create!({:amount => sender_credit_amount,
-                                            :remaining_amount => sender_credit_amount,
-                                            :reason => "Affiliate: #{event}", :user => sender, email: sender.email}, :without_protection => true)
+                                             :remaining_amount => sender_credit_amount,
+                                             :reason => "Affiliate: #{event}", :user => sender, email: sender.email}, :without_protection => true)
         log_event recipient.affiliate_partner, sender, credit, event
+
+        ActiveSupport::Notifications.instrument('spree.affiliate.create_credits.sender', {credit: credit, recipient: recipient , event: event, sender: sender }.merge(extra))
       end
 
 
       #check if affiliate should recevied credit on sign up
       if recipient_credit_amount = SpreeAffiliate::Config["recipient_credit_on_#{event}_amount".to_sym] and recipient_credit_amount.to_f > 0
         credit = Spree::StoreCredit.create!({:amount => recipient_credit_amount,
-                                            :remaining_amount => recipient_credit_amount,
-                                            :reason => "Affiliate: #{event}", :user => recipient, email: recipient.email}, :without_protection => true)
+                                             :remaining_amount => recipient_credit_amount,
+                                             :reason => "Affiliate: #{event}", :user => recipient, email: recipient.email}, :without_protection => true)
         log_event recipient.affiliate_partner, recipient, credit, event
+        ActiveSupport::Notifications.instrument('spree.affiliate.create_credits.sender', {credit: credit, recipient: recipient , event: event, sender: sender}.merge(extra))
+        # fire_event('spree.affiliate.create_credits.recipient', credit: credit, recipient: recipient, event: event )
       end
 
     end
