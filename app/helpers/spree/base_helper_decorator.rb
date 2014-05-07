c7e89428c327e76927f869dd1865f419d338931d
Mumo Carlos <mumo.crls@gmail.com>
2014-05-07 03:45:37 -0500

    Spree 2-2-stable compatibility
    * Adds translations and minor refactor on base helper decorator

--- app/helpers/spree/base_helper_decorator.rb
+++ app/helpers/spree/base_helper_decorator.rb
@@ -1,6 +1,6 @@
 Spree::BaseHelper.class_eval do
   def referral_url(referrer)
-    root_url(:ref_id => referrer.ref_id)
+    root_url(ref_id: referrer.ref_id)
   end
 
   def link_to_referral_url(referrer, options = {})
