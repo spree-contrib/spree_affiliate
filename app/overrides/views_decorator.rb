0aad8cf2f152c62c8dd6bb3164cf315817d7e4b8
Mumo Carlos <mumo.crls@gmail.com>
2014-05-07 03:42:57 -0500

    Spree 2-2-stable compatibility
    * Moves overrides to use deface dsl

--- app/overrides/views_decorator.rb
+++
@@ -1,9 +0,0 @@
-Deface::Override.new(:virtual_path => "spree/admin/shared/_configuration_menu",
-		 :name => "affiliates_admin_configuration_menu",
-     :insert_bottom => "[data-hook='admin_configurations_sidebar_menu']",
-     :text => "<%= configurations_sidebar_menu_item t(:affiliate_settings), spree.admin_affiliate_settings_path %>")
-
-Deface::Override.new(:virtual_path => "spree/users/show",
-		 :name => "converted_account_my_orders_69371225",
-		 :insert_before => "[data-hook='account_my_orders'], #account_my_orders[data-hook]",
-		 :partial => "spree/users/affiliate")
