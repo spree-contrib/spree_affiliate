6c52beeb7810bf473e3123c2e2a8fd0c8c2aa4f0
Mumo Carlos <mumo.crls@gmail.com>
2014-05-07 03:44:20 -0500

    Spree 2-2-stable compatibility
    * Retrieves affiliates on before_action

--- app/controllers/spree/users_controller_decorator.rb
+++ app/controllers/spree/users_controller_decorator.rb
@@ -1,6 +1,14 @@
 Spree::UserRegistrationsController.class_eval do
   include AffiliateCredits
 
-  after_filter :check_affiliate, :only => :create
+  before_action :find_affiliates, only: :show
+  after_action :check_affiliate, only: :create
+
+  private
+
+  def find_affiliates
+    @affiliates ||= current_spree_user.affiliates
+  end
 
 end
+
